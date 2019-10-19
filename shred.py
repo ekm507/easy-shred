0563b35b10fab716d64b5a12055fc352251c481f
Erfan Kheyrollahi <ekm507@gmail.com>
2019-10-19 18:32:07 +0330

    add filesize functtion using os lib

--- shred.py
+++ shred.py
@@ -2,9 +2,11 @@
 import os
 
 def shred(filename):
-    size = getFileSize(filename)
+    size = os.stat(filename).st_size
 
     for i in range(size):
         writeInFile(filename, 0)
 
+    print(size)
     os.remove(filename)
+
